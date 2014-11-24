6f4a5ff1eb2ed24661f20a2db2189f70e5d5d6ec
Essam <essam.a0@gmail.com>
2014-11-24 10:39:13 +0800

    Add erb support

--- lib/constantizer.rb
+++ lib/constantizer.rb
@@ -1,10 +1,11 @@
 require 'constantizer/version'
+require 'erb'
 
 module Constantizer
 
   def self.load!(path)
     @path = path
-    Dir.glob(File.join(path,'/*.yml')).each { |file| YAML.load_file(file).each { |k, v| Object.const_set k.upcase, v } }
+    Dir.glob(File.join(path,'/*.yml')).each { |file| YAML.load(ERB.new(IO.read(file)).result).each { |k, v| Object.const_set k.upcase, v } }
   end
 
   def self.reload!
